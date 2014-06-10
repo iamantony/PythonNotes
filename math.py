69b0eb4ff847648502a946d95784dac424927214
an.cherepanov <an.cherepanov@tensor.ru>
2014-06-10 17:30:28 +0600

    Tips: repeat string several times; pow number

---
+++ math.py
@@ -0,0 +1,11 @@
+__author__ = 'Anotny Cherepanov'
+
+
+def rise_number_to_power():
+    value = 2
+    powered_value = 2 ** 10
+    print("Original value :", value)
+    print("Powered value :", powered_value)
+
+
+rise_number_to_power()
---
+++ strings.py
@@ -0,0 +1,13 @@
+__author__ = 'Anotny Cherepanov'
+
+
+def repeat_string():
+    """ Repeat string several times simply multiplying it by some number """
+    string = "And Again "
+    long_string = string * 5
+    print("Original string :", string)
+    print("Repeated string :", long_string)
+
+
+repeat_string()
+
